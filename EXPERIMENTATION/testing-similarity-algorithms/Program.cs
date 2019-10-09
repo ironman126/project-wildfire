aff35d912d0bf79d72bb6a89f08d29a4896262e9
Alex <Alex>
2019-10-09 13:34:08 +0100

    Set up basic folders and initialised c# project for testing

---
+++ EXPERIMENTATION/testing-similarity-algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testing_similarity_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
