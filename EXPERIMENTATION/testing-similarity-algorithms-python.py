733df846bf1c391e797f89d9d64921487adbd71c
Alex <AlexAndHisScripts>
2019-10-09 16:38:02 +0100

    Added functional python script to get difference between datasets in percentage. If we have trouble with ML, we can always make something like this, and we can also support the ML with it.

---
+++ EXPERIMENTATION/testing-similarity-algorithms-python.py
@@ -0,0 +1,42 @@
+from math import *
+def percentage(x):
+    return (str(x * 100) + "%")
+
+def square_rooted(x):
+ 
+    return round(sqrt(sum([a*a for a in x])),3)
+
+def cosine_similarity(x,y):
+ 
+    numerator = sum(a*b for a,b in zip(x,y))
+    denominator = square_rooted(x)*square_rooted(y)
+    return round(numerator/float(denominator),3)
+
+def average(array):
+    average = 0
+    for i in array:
+        average = average + i
+
+    average = average / len(array)
+    return average
+
+
+                         
+wildfireDataset = [[1, 5, 4, 60], [3, 3, 7, 45], [2, 6, 3, 75], [2, 4, 8, 90], [1, 3, 7, 100], [1, 6, 3, 30], [4, 10, 4, 90], [1, 4, 5, 50], [3, 7, 6, 62], [1, 5, 4, 60]]
+
+currentDataset = []
+
+while True:
+    if len(currentDataset) > 3:
+        break
+    currentInput = int(input("Add to dataset:"))
+    currentDataset.append(currentInput)
+    
+results = []
+for fire in wildfireDataset:
+    similarity = cosine_similarity(currentDataset, fire)
+    results.append(similarity)
+
+print(percentage(average(results)))
+
+
