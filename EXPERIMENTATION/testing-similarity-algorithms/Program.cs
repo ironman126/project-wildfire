ebe4ab2b33de62fe2b988f90e504c26eda990abd
Alex <Alex>
2019-10-09 13:56:59 +0100

    I started making a test of how feasible basic dataset comparing was but because I am unfamiliar with that part of C # I have stopped and will do in python. I am committing now because I have to go to lesson and I might not be on this PC next time I come back to this.

--- EXPERIMENTATION/testing-similarity-algorithms/Program.cs
+++ EXPERIMENTATION/testing-similarity-algorithms/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+/*
 
+
+
+DO NOT USE THIS SCRIPT. DUE TO ME BEING LAUGHABLY AWFUL AT C# I AM REMAKING IN PYTHON. IT DOESN'T WORK.
+
+
+
+*/
 namespace testing_similarity_algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          int [,] wildfires = new int [5,4] { // [row, column]
+             {80, 9, 2, 178} ,
+             {97, 5, 15, 345} ,  //  Pretend first column is heat, second humidity, third cloud cover, fourth wind direction. */
+             {300, 1, 7, 50} ,
+             {150, 4, 3, 20} ,
+             {120, 5, 30, 120} ,
+            };
+
+            int [,] CurrentData = new int [1, 4] {
+              {140, 3, 8, 50}
+            };
+
+            foreach(int i in wildfires) {
+              Console.WriteLine("DEPRECATED");
+            }
+
         }
     }
 }
